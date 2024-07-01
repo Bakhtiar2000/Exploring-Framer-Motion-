b80a8f0c4fc91839211dca7e3e0fb4c2fd3ad022
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-07-02 01:57:19 +0600

    fetched using tanstackQuery

---
+++ src/api/admin/service/service.api.ts
@@ -0,0 +1,7 @@
+export const getServices = async () => {
+  const res = await fetch("http://localhost:5000/api/v1/services").then((res) =>
+    res.json()
+  );
+  if (!res.success) throw new Error("Something went wrong!");
+  return res;
+};
--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,7 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getServices } from "@/api/admin/service/service.api";
+import { useQuery } from "@tanstack/react-query";
+
 const ServiceList = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["services"],
+    queryFn: getServices,
+  });
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Something went wrong!</p>;
+
   return (
     <div>
-      <p>Service List</p>
+      {data.data.map((item: any, index: number) => (
+        <h2 key={index}>{item.name}</h2>
+      ))}
     </div>
   );
 };
